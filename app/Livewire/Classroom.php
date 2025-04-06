25b7bd50cff730eefbc1cfd7ddeb17a93d7d4478
MilliDev <vidjanmaximiano@gmail.com>
2025-04-06 08:04:38 +0100

    modifying classroom to be able to update teachers->class

--- app/Livewire/Classroom.php
+++ app/Livewire/Classroom.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Models\Classe;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Classroom extends Component
@@ -12,6 +14,8 @@ class Classroom extends Component
 
     public $class_name;
 
+    public $selectedId;
+
     public function createClassroom()
     {
 
@@ -19,7 +23,6 @@ class Classroom extends Component
             'class_name' => 'required|string|max:255',
             'selectedTeachers' => 'required|array',
         ]);
-
         try {
             DB::transaction(function () {
                 // Create the classroom
@@ -29,6 +32,50 @@ class Classroom extends Component
 
                 // Attach teachers (will only execute if class creation succeeded)
                 $class->teachers()->attach($this->selectedTeachers);
+            });
+
+            return redirect('/classroom')->with('message', 'Class Created'); // or wherever you want to redirect
+
+        } catch (\Exception $e) {
+
+            return redirect('/classroom')->with('error', 'Class Not Created: '.$e->getMessage()); // Preserves form input on error
+        }
+    }
+
+    #[On('update-created')]
+    public function select($id)
+    {
+
+        $cleanId = strtolower(trim($id));
+        $this->selectedId = $cleanId;
+        $classroom = Classe::with('teachers')->where('id', $cleanId)->first();
+        $this->class_name = $classroom->name;
+
+        $this->selectedTeachers = $classroom->teachers->pluck('id')->toArray();
+
+    }
+
+    public function update()
+    {
+
+        $record = \App\Models\Classe::find($this->selectedId);
+        $record->name = $this->class_name;
+
+        $this->validate([
+            'class_name' => 'required|string|max:255',
+            'selectedTeachers' => 'required|array',
+        ]);
+
+        try {
+            DB::transaction(function () {
+                // Create the classroom
+                $class = \App\Models\Classe::find($this->selectedId);
+                $class->update([
+                    'name' => $this->class_name,
+                ]);
+
+                // Attach teachers (will only execute if class creation succeeded)
+                $class->teachers()->sync($this->selectedTeachers);
             });
 
             return redirect('/classroom')->with('message', 'Class Created'); // or wherever you want to redirect
--- resources/views/livewire/classroom.blade.php
+++ resources/views/livewire/classroom.blade.php
@@ -160,47 +160,9 @@
         </div>
     </div>
     <!-- create classroom ---->
-    <div>
-        @foreach ($teachers as $teacher)
-            <ul wire:key={{ $teacher->id }}>
-                <li>{{ $teacher->user->first_name }}</li>
-            </ul>
-        @endforeach
-    </div>
-    <div class="pb-4 overflow-x-auto ">
-        <div class="inline-block min-w-full align-middle">
-            <div class="overflow-hidden border border-gray-300 rounded-lg">
-                <table class="min-w-full table-auto rounded-xl">
-                    <thead class="sticky top-0 z-10">
-                        <tr class="bg-gray-50">
-                            <th scope="col"
-                                class="p-5 text-center whitespace-nowrap text-sm leading-6 font-semibold text-gray-900 capitalize min-w-[150px]">
-                                Désignations</th>
-                            <th scope="col"
-                                class="p-5 text-sm font-semibold leading-6 text-center text-gray-900 capitalize whitespace-nowrap">
-                                Quantité Stock</th>
-                        </tr>
-                    </thead>
-                    <tbody class="divide-y divide-gray-300 ">
-                        @foreach ($teachers as $teacher)
-                            <tr class="transition-all duration-500 bg-white hover:bg-gray-50">
-                                <td
-                                    class="p-5 text-lg font-medium leading-6 text-center text-gray-900 whitespace-nowrap">
-                                    {{ $teacher->user->first_name }} </td>
-                                <td
-                                    class="p-5 text-lg font-medium leading-6 text-center text-gray-900 whitespace-nowrap">
-                                    <span
-                                        class="px-5 bg-green-500 border rounded-full">{{ $teacher->user->last_name }}</span>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
 
-    <div class="overflow-x-auto">
+
+    <div class="overflow-x-auto border border-gray-300 rounded-lg">
         <table class="min-w-full bg-white">
             <thead>
                 <tr>
@@ -233,12 +195,156 @@
                             @endforeach
                         </td>
                         <td class="px-4 py-2 border-b border-gray-200">
-                            <a href="#" class="mr-2 text-blue-600 hover:text-blue-900">Edit</a>
-                            <form action="#" method="POST" class="inline">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="text-red-600 hover:text-red-900">Delete</button>
-                            </form>
+                            <div class="" x-data="{ modelOpen2: false }">
+                                <button wire:click="$dispatch('update-created', { id:' {{ $classroom->id }}' })"
+                                    @click="modelOpen2 = !modelOpen2"
+                                    class="flex items-center space-x-2 text-blue-600 hover:text-blue-800">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-2 h-2" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path
+                                            d="M17.414 2.586a2 2 0 010 2.828l-10 10a2 2 0 01-.707.414l-4 1a1 1 0 01-1.265-1.265l1-4a2 2 0 01.414-.707l10-10a2 2 0 012.828 0zm-3.121 2.121L4 15l-.707 2.828L6.828 16l10.293-10.293-3.828-3.828z" />
+                                    </svg>
+                                    <span>{{ __('Edit') }}</span>
+                                </button>
+
+                                <div x-show="modelOpen2" class="fixed inset-0 z-50 overflow-y-auto"
+                                    aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                                    <div
+                                        class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+                                        <div x-cloak @click="modelOpen2 = false" x-show="modelOpen2"
+                                            x-transition:enter="transition ease-out duration-300 transform"
+                                            x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+                                            x-transition:leave="transition ease-in duration-200 transform"
+                                            x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+                                            class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40"
+                                            aria-hidden="true"></div>
+
+                                        <div x-cloak x-show="modelOpen2"
+                                            x-transition:enter="transition ease-out duration-300 transform"
+                                            x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                            x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                                            x-transition:leave="transition ease-in duration-200 transform"
+                                            x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                                            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                            class="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
+                                            <div class="flex items-center justify-between space-x-4">
+                                                <h1 class="text-xl font-medium text-gray-800 ">
+                                                    {{ __('Create a new classroom') }}</h1>
+
+                                                <button @click="modelOpen2 = false"
+                                                    class="text-gray-600 focus:outline-none hover:text-gray-700">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"
+                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+
+                                            <p class="mt-2 text-sm text-gray-500 ">
+                                                {{ __('Add teachers to classroom') }}
+                                            </p>
+
+                                            <form class="mt-5" wire:submit.prevent="update">
+
+                                                <div>
+                                                    <label for="class_name"
+                                                        class="block text-sm text-gray-700 capitalize dark:text-gray-200">{{ __('Classroom Name') }}</label>
+                                                    <input placeholder="CE2" type="text" wire:model="class_name"
+                                                        class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                </div>
+                                                <h1
+                                                    class="block mt-5 text-sm text-gray-700 capitalize dark:text-gray-200">
+                                                    {{ __('Select teachers') }}
+                                                    <div class="mt-5 space-y-2">
+                                                        @foreach ($teachers as $teacher)
+                                                            <label
+                                                                class="flex items-center p-2 space-x-2 border border-gray-200 rounded-md shadow-lg">
+                                                                <input type="checkbox" wire:model="selectedTeachers"
+                                                                    value="{{ $teacher->id }}"
+                                                                    class="text-indigo-600 border-gray-300 rounded shadow-sm">
+                                                                <span>{{ $teacher->user->first_name }}</span>
+                                                                <span>{{ $teacher->user->last_name }}</span>
+                                                            </label>
+                                                        @endforeach
+                                                    </div>
+
+                                                    <div class="mt-4">
+                                                        <h1 class="text-xs font-medium text-gray-400 uppercase">
+                                                            Permissions</h1>
+
+                                                        <div class="mt-4 space-y-5">
+                                                            <div class="flex items-center space-x-3 cursor-pointer"
+                                                                x-data="{ show: true }" @click="show =!show">
+                                                                <div class="relative w-10 h-5 transition duration-200 ease-linear rounded-full"
+                                                                    :class="[show ? 'bg-indigo-500' : 'bg-gray-300']">
+                                                                    <label for="show" @click="show =!show"
+                                                                        class="absolute left-0 w-5 h-5 mb-2 transition duration-100 ease-linear transform bg-white border-2 rounded-full cursor-pointer"
+                                                                        :class="[show ? 'translate-x-full border-indigo-500' :
+                                                                            'translate-x-0 border-gray-300'
+                                                                        ]"></label>
+                                                                    <input type="checkbox" name="show"
+                                                                        class="hidden w-full h-full rounded-full appearance-none active:outline-none focus:outline-none" />
+                                                                </div>
+
+                                                                <p class="text-gray-500">Can make task</p>
+                                                            </div>
+
+                                                            <div class="flex items-center space-x-3 cursor-pointer"
+                                                                x-data="{ show: false }" @click="show =!show">
+                                                                <div class="relative w-10 h-5 transition duration-200 ease-linear rounded-full"
+                                                                    :class="[show ? 'bg-indigo-500' : 'bg-gray-300']">
+                                                                    <label for="show" @click="show =!show"
+                                                                        class="absolute left-0 w-5 h-5 mb-2 transition duration-100 ease-linear transform bg-white border-2 rounded-full cursor-pointer"
+                                                                        :class="[show ? 'translate-x-full border-indigo-500' :
+                                                                            'translate-x-0 border-gray-300'
+                                                                        ]"></label>
+                                                                    <input type="checkbox" name="show"
+                                                                        class="hidden w-full h-full rounded-full appearance-none active:outline-none focus:outline-none" />
+                                                                </div>
+
+                                                                <p class="text-gray-500">Can delete task</p>
+                                                            </div>
+
+                                                            <div class="flex items-center space-x-3 cursor-pointer"
+                                                                x-data="{ show: true }" @click="show =!show">
+                                                                <div class="relative w-10 h-5 transition duration-200 ease-linear rounded-full"
+                                                                    :class="[show ? 'bg-indigo-500' : 'bg-gray-300']">
+                                                                    <label for="show" @click="show =!show"
+                                                                        class="absolute left-0 w-5 h-5 mb-2 transition duration-100 ease-linear transform bg-white border-2 rounded-full cursor-pointer"
+                                                                        :class="[show ? 'translate-x-full border-indigo-500' :
+                                                                            'translate-x-0 border-gray-300'
+                                                                        ]"></label>
+                                                                    <input type="checkbox" name="show"
+                                                                        class="hidden w-full h-full rounded-full appearance-none active:outline-none focus:outline-none" />
+                                                                </div>
+
+                                                                <p class="text-gray-500">Can edit task</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="flex justify-end mt-6">
+                                                        <button type="submit"
+                                                            class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                                                            {{ __('Update') }}
+                                                        </button>
+                                                    </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <button class="flex items-center space-x-2 text-red-600 hover:text-red-800">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-2 h-2" viewBox="0 0 20 20"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                        d="M6 2a1 1 0 011-1h6a1 1 0 011 1h3a1 1 0 110 2H2a1 1 0 110-2h3zm3 4a1 1 0 00-1 1v7a1 1 0 102 0V7a1 1 0 00-1-1zm4 0a1 1 0 00-1 1v7a1 1 0 102 0V7a1 1 0 00-1-1zM4 6a1 1 0 011 1v7a1 1 0 102 0V7a1 1 0 00-1-1H4zm10 0a1 1 0 011 1v7a1 1 0 102 0V7a1 1 0 00-1-1h-1z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                <span>{{ __('Delete') }}</span>
+                            </button>
                         </td>
                     </tr>
                 @endforeach
