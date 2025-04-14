70ee818d0c0922f923df3af546f863aa12f5fce3
MilliDev <vidjanmaximiano@gmail.com>
2025-04-14 13:30:08 +0100

    adding dropdown,checkbox to assign-subject

--- resources/views/livewire/assign-subject.blade.php
+++ resources/views/livewire/assign-subject.blade.php
@@ -87,79 +87,59 @@
                                 class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40"></textarea>
                         </div>
 
-                        <div class="mt-4">
-                            <select wire:model="class_id" class="block w-full p-2 border border-gray-300 rounded">
-                                <option value="">Select Class</option>
-                                @foreach ($classes as $class)
-                                    <option value="{{ $class->id }}">{{ $class->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                        <div class="mt-4">
-                            <select wire:model="teacher_id" class="block w-full p-2 border border-gray-300 rounded">
-                                <option value="">Select Teacher</option>
-                                @foreach ($teachers as $teacher)
-                                    <option value="{{ $teacher->id }}">{{ $teacher->user->first_name }}
-                                        {{ $teacher->user->last_name }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                        <div class="mt-4">
-                            <h1 class="text-xs font-medium text-gray-400 uppercase">Permissions</h1>
-
-                            <div class="mt-4 space-y-5">
-                                <div class="flex items-center space-x-3 cursor-pointer" x-data="{ show: true }"
-                                    @click="show =!show">
-                                    <div class="relative w-10 h-5 transition duration-200 ease-linear rounded-full"
-                                        :class="[show ? 'bg-indigo-500' : 'bg-gray-300']">
-                                        <label for="show" @click="show =!show"
-                                            class="absolute left-0 w-5 h-5 mb-2 transition duration-100 ease-linear transform bg-white border-2 rounded-full cursor-pointer"
-                                            :class="[show ? 'translate-x-full border-indigo-500' :
-                                                'translate-x-0 border-gray-300'
-                                            ]"></label>
-                                        <input type="checkbox" name="show"
-                                            class="hidden w-full h-full rounded-full appearance-none active:outline-none focus:outline-none" />
-                                    </div>
-
-                                    <p class="text-gray-500">Can make task</p>
-                                </div>
-
-                                <div class="flex items-center space-x-3 cursor-pointer" x-data="{ show: false }"
-                                    @click="show =!show">
-                                    <div class="relative w-10 h-5 transition duration-200 ease-linear rounded-full"
-                                        :class="[show ? 'bg-indigo-500' : 'bg-gray-300']">
-                                        <label for="show" @click="show =!show"
-                                            class="absolute left-0 w-5 h-5 mb-2 transition duration-100 ease-linear transform bg-white border-2 rounded-full cursor-pointer"
-                                            :class="[show ? 'translate-x-full border-indigo-500' :
-                                                'translate-x-0 border-gray-300'
-                                            ]"></label>
-                                        <input type="checkbox" name="show"
-                                            class="hidden w-full h-full rounded-full appearance-none active:outline-none focus:outline-none" />
-                                    </div>
-
-                                    <p class="text-gray-500">Can delete task</p>
-                                </div>
-
-                                <div class="flex items-center space-x-3 cursor-pointer" x-data="{ show: true }"
-                                    @click="show =!show">
-                                    <div class="relative w-10 h-5 transition duration-200 ease-linear rounded-full"
-                                        :class="[show ? 'bg-indigo-500' : 'bg-gray-300']">
-                                        <label for="show" @click="show =!show"
-                                            class="absolute left-0 w-5 h-5 mb-2 transition duration-100 ease-linear transform bg-white border-2 rounded-full cursor-pointer"
-                                            :class="[show ? 'translate-x-full border-indigo-500' :
-                                                'translate-x-0 border-gray-300'
-                                            ]"></label>
-                                        <input type="checkbox" name="show"
-                                            class="hidden w-full h-full rounded-full appearance-none active:outline-none focus:outline-none" />
-                                    </div>
-
-                                    <p class="text-gray-500">Can edit task</p>
+                        <div class="w-full mt-4 ">
+                            <div class="" x-data="{ show: false }">
+                                <a href="#" x-on:click.prevent="show = !show"
+                                    class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                    <span class="inline-block">{{ __('Select classes') }}</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        class="inline-block w-4 h-4 transition duration-150 transform stroke-current"
+                                        x-bind:class="{ 'rotate-180': show }">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M19 9l-7 7-7-7" />
+                                    </svg>
+                                </a>
+                                <div x-show.transition="show"
+                                    class="relative z-20 flex flex-col w-full px-4 py-8 mt-1 bg-white border border-gray-600 rounded whitespace-nowrap">
+                                    @foreach ($classes as $class)
+                                        <div>
+                                            <input wire:model='class_id' type="checkbox" name="type[]"
+                                                value="{{ $class->id }}"
+                                                class="inline-block mr-2" />{{ $class->name }}
+                                        </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
+
+                        <div class="w-full mt-4 ">
+                            <div class="" x-data="{ show: false }">
+                                <a href="#" x-on:click.prevent="show = !show"
+                                    class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                    <span class="inline-block">{{ __('Select teachers') }}</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        class="inline-block w-4 h-4 transition duration-150 transform stroke-current"
+                                        x-bind:class="{ 'rotate-180': show }">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M19 9l-7 7-7-7" />
+                                    </svg>
+                                </a>
+                                <div x-show.transition="show"
+                                    class="relative z-20 flex flex-col w-full px-4 py-8 mt-1 bg-white border border-gray-600 rounded whitespace-nowrap">
+                                    @foreach ($teachers as $item)
+                                        <div>
+                                            <input wire:model='teacher_id' type="checkbox" name="type[]"
+                                                value="{{ $item->id }}"
+                                                class="inline-block mr-2" />{{ $item->user->first_name }}
+                                            {{ $item->user->last_name }}
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+
 
                         <div class="flex justify-end mt-6">
                             <button type="submit"
@@ -210,8 +190,7 @@
                         </td>
                         <td class="px-4 py-2 border-b border-gray-200">
                             <div class="" x-data="{ modelOpen2: false }">
-                                <button wire:click="$dispatch('update-created', { id:' {{ $classroom->id }}' })"
-                                    @click="modelOpen2 = !modelOpen2"
+                                <button @click="modelOpen2 = !modelOpen2"
                                     class="flex items-center space-x-2 text-blue-600 hover:text-blue-800">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="w-2 h-2" viewBox="0 0 20 20"
                                         fill="currentColor">
@@ -270,58 +249,14 @@
                                                 </div>
 
                                                 <div class="mt-4">
-                                                    <h1 class="text-xs font-medium text-gray-400 uppercase">
-                                                        Permissions</h1>
-
-                                                    <div class="mt-4 space-y-5">
-                                                        <div class="flex items-center space-x-3 cursor-pointer"
-                                                            x-data="{ show: true }" @click="show =!show">
-                                                            <div class="relative w-10 h-5 transition duration-200 ease-linear rounded-full"
-                                                                :class="[show ? 'bg-indigo-500' : 'bg-gray-300']">
-                                                                <label for="show" @click="show =!show"
-                                                                    class="absolute left-0 w-5 h-5 mb-2 transition duration-100 ease-linear transform bg-white border-2 rounded-full cursor-pointer"
-                                                                    :class="[show ? 'translate-x-full border-indigo-500' :
-                                                                        'translate-x-0 border-gray-300'
-                                                                    ]"></label>
-                                                                <input type="checkbox" name="show"
-                                                                    class="hidden w-full h-full rounded-full appearance-none active:outline-none focus:outline-none" />
-                                                            </div>
-
-                                                            <p class="text-gray-500">Can make task</p>
-                                                        </div>
-
-                                                        <div class="flex items-center space-x-3 cursor-pointer"
-                                                            x-data="{ show: false }" @click="show =!show">
-                                                            <div class="relative w-10 h-5 transition duration-200 ease-linear rounded-full"
-                                                                :class="[show ? 'bg-indigo-500' : 'bg-gray-300']">
-                                                                <label for="show" @click="show =!show"
-                                                                    class="absolute left-0 w-5 h-5 mb-2 transition duration-100 ease-linear transform bg-white border-2 rounded-full cursor-pointer"
-                                                                    :class="[show ? 'translate-x-full border-indigo-500' :
-                                                                        'translate-x-0 border-gray-300'
-                                                                    ]"></label>
-                                                                <input type="checkbox" name="show"
-                                                                    class="hidden w-full h-full rounded-full appearance-none active:outline-none focus:outline-none" />
-                                                            </div>
-
-                                                            <p class="text-gray-500">Can delete task</p>
-                                                        </div>
-
-                                                        <div class="flex items-center space-x-3 cursor-pointer"
-                                                            x-data="{ show: true }" @click="show =!show">
-                                                            <div class="relative w-10 h-5 transition duration-200 ease-linear rounded-full"
-                                                                :class="[show ? 'bg-indigo-500' : 'bg-gray-300']">
-                                                                <label for="show" @click="show =!show"
-                                                                    class="absolute left-0 w-5 h-5 mb-2 transition duration-100 ease-linear transform bg-white border-2 rounded-full cursor-pointer"
-                                                                    :class="[show ? 'translate-x-full border-indigo-500' :
-                                                                        'translate-x-0 border-gray-300'
-                                                                    ]"></label>
-                                                                <input type="checkbox" name="show"
-                                                                    class="hidden w-full h-full rounded-full appearance-none active:outline-none focus:outline-none" />
-                                                            </div>
-
-                                                            <p class="text-gray-500">Can edit task</p>
-                                                        </div>
-                                                    </div>
+                                                    <select wire:model="class_id"
+                                                        class="block w-full p-2 border border-gray-300 rounded">
+                                                        <option value="">Select Class</option>
+                                                        @foreach ($classes as $class)
+                                                            <option value="{{ $class->id }}">{{ $class->name }}
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
                                                 </div>
 
                                                 <div class="flex justify-end mt-6">
