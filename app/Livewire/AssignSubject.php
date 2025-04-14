9b8cb067ddc11247e506ee77bb8f12d1cd5780f5
MilliDev <vidjanmaximiano@gmail.com>
2025-04-14 13:30:32 +0100

    creating function createSubject()

--- app/Livewire/AssignSubject.php
+++ app/Livewire/AssignSubject.php
@@ -2,30 +2,59 @@
 
 namespace App\Livewire;
 
+use App\Models\Subject;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class AssignSubject extends Component
 {
-    public $class_id;
+    public $class_id = [];
 
     public $subject_name;
 
     public $subject_description;
 
-    public $teacher_id;
+    public $teacher_id = [];
 
     public function createSubject()
     {
+
         $this->validate([
             'subject_name' => 'required|string|max:255',
             'subject_description' => 'required|string|max:255',
+            'class_id' => 'required|array',
+            'teacher_id' => 'required|array',
         ]);
         try {
-            \App\Models\Subject::create([
-                'name' => $this->subject_name,
-                'description' => $this->subject_description,
-            ]);
+            DB::transaction(function () {
+                // Check if the subject already exists
+                $existingSubject = Subject::where('name', $this->subject_name)->first();
+
+                if ($existingSubject) {
+                    // Throw an exception to stop everything
+                    throw ValidationException::withMessages([
+                        'subject_name' => 'The subject already exists.',
+                    ]);
+                }
+
+                // Create the subject if it doesn't exist
+                $subject = Subject::create([
+                    'name' => $this->subject_name,
+                    'description' => $this->subject_description,
+                ]);
+
+                foreach ($this->class_id as $class) {
+                    foreach ($this->teacher_id as $teacher) {
+                        ClassSubjectTeacher::create([
+                            'classroom_id' => $class,
+                            'subject_id' => $subject->id,
+                            'teacher_id' => $teacher,
+                        ]);
+                    }
+                }
+            });
 
             return redirect('/assign-subject')->with('message', 'Subject Assigned'); // or wherever you want to redirect
 
@@ -39,7 +68,7 @@ class AssignSubject extends Component
     public function render()
     {
         return view('livewire.assign-subject', [
-            'subjects' => \App\Models\ClassSubjectTeacher::with(['teacher', 'subject', 'classroom'])->get(),
+
             'teachers' => \App\Models\Teachers::all(),
             'subjects' => \App\Models\Subject::all(),
             'classes' => \App\Models\Classe::all(),
