f6997014835fcc1590c0063142601160c90a441d
derick maximiano vidjan <vidjanmaximiano@gmail.com>
2025-04-15 22:21:23 +0100

    creating lang-subject.php

---
+++ lang/fr/subjects.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Subject Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during subjects creation for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'creer' => 'Créer une matière',
+    'Modifier' => 'Modifier une matière',
+    'Nom' => 'Nom de la matière',
+    'Code' => 'Code de la matière',
+    'Description' => 'Description de la matière',
+    'Nom de la matière' => 'Nom de la matière',
+
+];
