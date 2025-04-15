b959e9f2cf1ad2d90ef438b24e2a47403ce79002
derick maximiano vidjan <vidjanmaximiano@gmail.com>
2025-04-15 22:22:12 +0100

    change to french

--- resources/views/livewire/assign-subject.blade.php
+++ resources/views/livewire/assign-subject.blade.php
@@ -36,7 +36,7 @@
                     clip-rule="evenodd" />
             </svg>
 
-            <span>{{ __('create a subject') }}</span>
+            <span>{{ __('Créer une matière') }}</span>
         </button>
 
         <div x-show="modelOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog"
@@ -56,7 +56,7 @@
                     x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     class="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
                     <div class="flex items-center justify-between space-x-4">
-                        <h1 class="text-xl font-medium text-gray-800 ">{{ __('Create a Subject') }}</h1>
+                        <h1 class="text-xl font-medium text-gray-800 ">{{ __('Créer une matière') }}</h1>
 
                         <button @click="modelOpen = false" class="text-gray-600 focus:outline-none hover:text-gray-700">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
@@ -68,21 +68,21 @@
                     </div>
 
                     <p class="mt-2 text-sm text-gray-500 ">
-                        {{ __('Add Subject') }}
+                        {{ __('Ajouter Matière') }}
                     </p>
 
                     <form class="mt-5" wire:submit="createSubject">
 
                         <div>
                             <label for="subject_name"
-                                class="block text-sm text-gray-700 capitalize dark:text-gray-200">{{ __('Subject Name') }}</label>
+                                class="block text-sm text-gray-700 capitalize dark:text-gray-200">{{ __('Nom') }}</label>
                             <input placeholder="Math" type="text" wire:model="subject_name"
                                 class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
                         </div>
 
                         <div class="mt-4">
                             <label for="subject_description"
-                                class="block text-sm text-gray-700 capitalize dark:text-gray-200">{{ __('Subject Description') }}</label>
+                                class="block text-sm text-gray-700 capitalize dark:text-gray-200">{{ __('Description') }}</label>
                             <textarea placeholder="Math is the study of numbers, shapes, and patterns." wire:model="subject_description"
                                 class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40"></textarea>
                         </div>
@@ -91,7 +91,7 @@
                             <div class="" x-data="{ show: false }">
                                 <a href="#" x-on:click.prevent="show = !show"
                                     class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
-                                    <span class="inline-block">{{ __('Select classes') }}</span>
+                                    <span class="inline-block">{{ __('Sélectionner classes') }}</span>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke="currentColor"
                                         class="inline-block w-4 h-4 transition duration-150 transform stroke-current"
@@ -117,7 +117,7 @@
                             <div class="" x-data="{ show: false }">
                                 <a href="#" x-on:click.prevent="show = !show"
                                     class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
-                                    <span class="inline-block">{{ __('Select teachers') }}</span>
+                                    <span class="inline-block">{{ __('Sélectionner enseignants') }}</span>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke="currentColor"
                                         class="inline-block w-4 h-4 transition duration-150 transform stroke-current"
@@ -144,7 +144,7 @@
                         <div class="flex justify-end mt-6">
                             <button type="submit"
                                 class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
-                                {{ __('Create Subject') }}
+                                {{ __('Validez') }}
                             </button>
                         </div>
                     </form>
@@ -160,11 +160,11 @@
                 <tr>
                     <th
                         class="px-4 py-2 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase border-b border-gray-200 bg-gray-50">
-                        {{ __('Subjects') }}
+                        {{ __('Matières') }}
                     </th>
                     <th
                         class="px-4 py-2 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase border-b border-gray-200 bg-gray-50">
-                        {{ __('Teachers') }}
+                        {{ __('Enseignants') }}
                     </th>
                     <th
                         class="px-4 py-2 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase border-b border-gray-200 bg-gray-50">
@@ -178,14 +178,14 @@
             </thead>
             <tbody>
                 @foreach ($class_subject_teachers as $subject_id => $items)
-                    <tr class="border">
+                    <tr class="border" wire:key="{{ $subject_id }}">
                         <td class="px-4 py-2 ">
                             {{ $items->first()->subject->name }}
                         </td>
                         <td class="px-4 py-2 ">
-                            @foreach ($items->unique('teacher_id') as $item)
-                                @if ($item->teacher && $item->teacher->user)
-                                    <ol wire:key="{{ $item->id }}">
+                            <ol>
+                                @foreach ($items->unique('teacher_id') as $item)
+                                    @if ($item->teacher && $item->teacher->user)
                                         <li>
                                             <span
                                                 class="inline-block px-2 py-1 mb-1 mr-1 text-xs text-blue-800 bg-blue-100 rounded-full">
@@ -193,10 +193,9 @@
                                                 {{ $item->teacher->user->last_name }}
                                             </span>
                                         </li>
-                                    </ol>
-                                @endif
-                            @endforeach
-
+                                    @endif
+                                @endforeach
+                            </ol>
                         </td>
                         <td class="px-4 py-2 ">
                             @foreach ($items->unique('classroom_id') as $item)
@@ -210,14 +209,14 @@
                         </td>
                         <td class="flex gap-2 px-4 py-2 ">
                             <div class="" x-data="{ modelOpen2: false }">
-                                <button @click="modelOpen2 = !modelOpen2"
+                                <button @click="modelOpen2 = !modelOpen2" wire:click="$dispatch('update-created', { id:'{{ $subject_id}}' })"
                                     class="flex items-center space-x-2 text-blue-600 hover:text-blue-800">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="w-2 h-2" viewBox="0 0 20 20"
                                         fill="currentColor">
                                         <path
                                             d="M17.414 2.586a2 2 0 010 2.828l-10 10a2 2 0 01-.707.414l-4 1a1 1 0 01-1.265-1.265l1-4a2 2 0 01.414-.707l10-10a2 2 0 012.828 0zm-3.121 2.121L4 15l-.707 2.828L6.828 16l10.293-10.293-3.828-3.828z" />
                                     </svg>
-                                    <span>{{ __('Edit') }}</span>
+                                    <span>{{ __('Modifier') }}</span>
                                 </button>
 
                                 <div x-show="modelOpen2" class="fixed inset-0 z-50 overflow-y-auto"
@@ -242,7 +241,7 @@
                                             class="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
                                             <div class="flex items-center justify-between space-x-4">
                                                 <h1 class="text-xl font-medium text-gray-800 ">
-                                                    {{ __('Create a new classroom') }}</h1>
+                                                    {{ __('Modifier une matière') }}</h1>
 
                                                 <button @click="modelOpen2 = false"
                                                     class="text-gray-600 focus:outline-none hover:text-gray-700">
@@ -256,34 +255,83 @@
                                             </div>
 
                                             <p class="mt-2 text-sm text-gray-500 ">
-                                                {{ __('Add teachers to classroom') }}
+                                                {{ __('Modifier une matière') }}
                                             </p>
 
                                             <form class="mt-5" wire:submit.prevent="update">
 
                                                 <div>
-                                                    <label for="class_name"
-                                                        class="block text-sm text-gray-700 capitalize dark:text-gray-200">{{ __('Classroom Name') }}</label>
-                                                    <input placeholder="CE2" type="text" wire:model="class_name"
+                                                    <label for="subject_name"
+                                                        class="block text-sm text-gray-700 capitalize dark:text-gray-200">{{ __('Nom') }}</label>
+                                                    <input placeholder="Math" type="text" wire:model="subject_name"
                                                         class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
                                                 </div>
-
+                        
                                                 <div class="mt-4">
-                                                    <select wire:model="class_id"
-                                                        class="block w-full p-2 border border-gray-300 rounded">
-                                                        <option value="">Select Class</option>
-                                                        @foreach ($classes as $class)
-                                                            <option value="{{ $class->id }}">
-                                                                {{ $class->name }}
-                                                            </option>
-                                                        @endforeach
-                                                    </select>
+                                                    <label for="subject_description"
+                                                        class="block text-sm text-gray-700 capitalize dark:text-gray-200">{{ __('Description') }}</label>
+                                                    <textarea placeholder="Math is the study of numbers, shapes, and patterns." wire:model="subject_description"
+                                                        class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40"></textarea>
                                                 </div>
-
+                        
+                                                <div class="w-full mt-4 ">
+                                                    <div class="" x-data="{ show: false }">
+                                                        <a href="#" x-on:click.prevent="show = !show"
+                                                            class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                            <span class="inline-block">{{ __('Sélectionner classes') }}</span>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                                stroke="currentColor"
+                                                                class="inline-block w-4 h-4 transition duration-150 transform stroke-current"
+                                                                x-bind:class="{ 'rotate-180': show }">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                                    d="M19 9l-7 7-7-7" />
+                                                            </svg>
+                                                        </a>
+                                                        <div x-show.transition="show"
+                                                            class="relative z-20 flex flex-col w-full px-4 py-8 mt-1 bg-white border border-gray-600 rounded whitespace-nowrap">
+                                                            @foreach ($classes as $class)
+                                                                <div>
+                                                                    <input wire:model='class_id' type="checkbox" name="type[]"
+                                                                        value="{{ $class->id }}"
+                                                                        class="inline-block mr-2" />{{ $class->name }}
+                                                                </div>
+                                                            @endforeach
+                                                        </div>
+                                                    </div>
+                                                </div>
+                        
+                                                <div class="w-full mt-4 ">
+                                                    <div class="" x-data="{ show: false }">
+                                                        <a href="#" x-on:click.prevent="show = !show"
+                                                            class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                                            <span class="inline-block">{{ __('Sélectionner enseignants') }}</span>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                                stroke="currentColor"
+                                                                class="inline-block w-4 h-4 transition duration-150 transform stroke-current"
+                                                                x-bind:class="{ 'rotate-180': show }">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                                    d="M19 9l-7 7-7-7" />
+                                                            </svg>
+                                                        </a>
+                                                        <div x-show.transition="show"
+                                                            class="relative z-20 flex flex-col w-full px-4 py-8 mt-1 bg-white border border-gray-600 rounded whitespace-nowrap">
+                                                            @foreach ($teachers as $item)
+                                                                <div>
+                                                                    <input wire:model='teacher_id' type="checkbox" name="type[]"
+                                                                        value="{{ $item->id }}"
+                                                                        class="inline-block mr-2" />{{ $item->user->first_name }}
+                                                                    {{ $item->user->last_name }}
+                                                                </div>
+                                                            @endforeach
+                                                        </div>
+                                                    </div>
+                                                </div>
+                        
+                        
                                                 <div class="flex justify-end mt-6">
                                                     <button type="submit"
                                                         class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
-                                                        {{ __('Update') }}
+                                                        {{ __('Mettre à jour') }}
                                                     </button>
                                                 </div>
                                             </form>
