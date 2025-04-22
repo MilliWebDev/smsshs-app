2bf727752772eb15bcfd0e776f3b67f884b742d8
MilliDev <vidjanmaximiano@gmail.com>
2025-04-22 22:20:00 +0100

    adding else and display message when there is no student.

--- resources/views/livewire/grade.blade.php
+++ resources/views/livewire/grade.blade.php
@@ -1,4 +1,4 @@
-<div class="p-6 bg-white rounded shadow mt-10">
+<div class="p-6 mt-10 bg-white rounded shadow">
 
     <div class="my-4">
         <label class="block mb-2 font-medium text-gray-600">{{ __('Sélectionner une classe et une matière') }}</label>
@@ -24,7 +24,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @if ($students)
+                    @if ($students && $students->count() > 0)
                         @foreach ($students as $student)
                             <tr class="border-t">
                                 <td class="p-2">{{ $student->first_name }}
@@ -33,12 +33,18 @@
                                     <td class="p-2">
                                         <input type="number" min="0" max="20"
                                             wire:change.debounce.500ms="saveGrade('{{ $student->id }}', '{{ $assignment->id }}', $event.target.value)"
-                                            class="w-20 px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200" />
+                                            class="w-20 px-3 py-2 transition duration-200 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" />
 
                                     </td>
                                 @endforeach
                             </tr>
                         @endforeach
+                    @else
+                        <tr>
+                            <td colspan="{{ $assignments ? $assignments->count() + 1 : 2 }}" class="p-2 text-center">
+                                {{ __('Aucun apprenant ou matière trouvé') }}.
+                            </td>
+                        </tr>
                     @endif
                 </tbody>
             </table>
