b5ca93d05f7f555149f5f0bff1e377b708415bc4
MilliDev <vidjanmaximiano@gmail.com>
2025-04-23 18:31:27 +0100

    adding admin gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,7 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Defines admin gate
+        Gate::define('admin-access', function ($user) {
+            // Check if the user's role is 'admin'
+            return $user->role === 'admin';
+        });
 
     }
 }
