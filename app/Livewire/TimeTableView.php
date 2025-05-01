90d8dfcae1981380fb2042e6eed255f1796ff849
MilliDev <vidjanmaximiano@gmail.com>
2025-05-01 14:27:22 +0100

    creating timetableview livewire

---
+++ app/Livewire/TimeTableView.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TimeTableView extends Component
+{
+    public function render()
+    {
+        return view('livewire.time-table-view');
+    }
+}
