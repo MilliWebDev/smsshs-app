44913de5df78f29df9a0a1a9a9551d6605bbf2f4
derick maximiano vidjan <vidjanmaximiano@gmail.com>
2025-05-05 18:00:57 +0100

    adding semesters form

--- app/Livewire/Assignmentview.php
+++ app/Livewire/Assignmentview.php
@@ -5,11 +5,12 @@ namespace App\Livewire;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 use App\Models\Assignment;
+use App\Models\Semester;
 use Livewire\Attributes\On;
 class Assignmentview extends Component
 {
 
-    public $title,$description,$selectedId;
+    public $title,$description,$selectedId,$title_semester,$start_date,$end_date;
 
     public function createAssignment()
     {
@@ -30,6 +31,27 @@ class Assignmentview extends Component
         return redirect('/assignmentview')->with('message', 'Devoir ajouté'); // or wherever you want to redirect
     }
 
+
+    public function createSemester()
+    {
+        $this->validate([
+            'title_semester' => 'required|string|max:255',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ]);
+
+        // Logic to create the semester
+        // For example, you can save it to the database or perform any other action
+
+        // Create the subject if it doesn't exist
+        $assignment = Semester::create([
+            'name' => $this->title_semester,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+        ]);
+
+        return redirect('/assignmentview')->with('message', 'Semestre ajouté'); // or wherever you want to redirect
+    }
 
     #[On('update-created')]
     public function select($id)
--- resources/views/livewire/assignmentview.blade.php
+++ resources/views/livewire/assignmentview.blade.php
@@ -27,7 +27,7 @@
         @endif
     </div>
 
-    <div class="flex justify-end w-full h-10" x-data="{ modelOpen: false }">
+    <div class="flex justify-end w-full h-10 gap-5" x-data="{ modelOpen: false, model2Open: false }">
         <button @click="modelOpen =!modelOpen"
             class="flex items-center justify-center px-3 py-2 space-x-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
@@ -37,6 +37,17 @@
             </svg>
 
             <span class='text-lg font-bold'>{{ __('Créer un devoir') }}</span>
+        </button>
+
+        <button @click="model2Open =!model2Open"
+            class="flex items-center justify-center px-3 py-2 space-x-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd"
+                    d=" M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                    clip-rule="evenodd" />
+            </svg>
+
+            <span class='text-lg font-bold'>{{ __('Créer un trimestre') }}</span>
         </button>
 
         <div x-show="modelOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog"
@@ -75,7 +86,7 @@
                         <div>
                             <label for="title"
                                 class="block text-lg font-bold text-gray-700 capitalize dark:text-gray-200">{{ __('Titre') }}</label>
-                            <input placeholder="Devoir de mathématiques" type="text" wire:model="title"
+                            <input placeholder="Devoir,interrogation1..." type="text" wire:model="title"
                                 class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
                             @error('title')
                                 <p class="mt-1 text-sm text-red-600">
@@ -94,6 +105,82 @@
                             @enderror
                         </div>
 
+                        <div class="flex justify-end mt-6">
+                            <button type="submit"
+                                class="px-3 py-2 text-lg font-bold tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                                {{ __('Validez') }}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div x-show="model2Open" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog"
+            aria-modal="true">
+            <div class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+                <div x-cloak @click="model2Open = false" x-show="model2Open"
+                    x-transition:enter="transition ease-out duration-300 transform" x-transition:enter-start="opacity-0"
+                    x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-200 transform"
+                    x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+                    class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true"></div>
+
+                <div x-cloak x-show="model2Open" x-transition:enter="transition ease-out duration-300 transform"
+                    x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                    x-transition:leave="transition ease-in duration-200 transform"
+                    x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                    x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    class="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
+                    <div class="flex items-center justify-between space-x-4">
+                        <h1 class="text-xl font-medium text-gray-800 ">{{ __('Créer un trimestre') }}</h1>
+
+                        <button @click="model2Open = false" class="text-gray-600 focus:outline-none hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </button>
+                    </div>
+
+                    <p class="mt-2 text-sm text-gray-500 ">
+                        {{ __('Ajouter un trimestre') }}
+                    </p>
+
+                    <form class="mt-5" wire:submit="createSemester">
+                        <div>
+                            <label for="title"
+                                class="block text-lg font-bold text-gray-700 capitalize dark:text-gray-200">{{ __('Titre du trimestre') }}</label>
+                            <input placeholder="Trimestre 1...." type="text" wire:model="title_semester"
+                                class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                            @error('title_semester')
+                                <p class="mt-1 text-sm text-red-600">
+                                    {{ __('Veillez entrer un titre') }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="mt-4">
+                            <label for="start_date"
+                                class="block text-lg font-bold text-gray-700 capitalize dark:text-gray-200">{{ __('Date de debut') }}</label>
+                                <input type="date" wire:model="start_date" name="start_date"
+                                class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                            @error('start_date')
+                                <p class="mt-1 text-sm text-red-600">
+                                    {{ __('Veillez entrer une date') }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="mt-4">
+                            <label for="end_date"
+                                class="block text-lg font-bold text-gray-700 capitalize dark:text-gray-200">{{ __('Date de fin') }}</label>
+                                <input type="date" wire:model="end_date" name="end_date"
+                                class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                            @error('end_date')
+                                <p class="mt-1 text-sm text-red-600">
+                                    {{ __('Veillez entrer une date') }}</p>
+                            @enderror
+                        </div>
                         <div class="flex justify-end mt-6">
                             <button type="submit"
                                 class="px-3 py-2 text-lg font-bold tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
